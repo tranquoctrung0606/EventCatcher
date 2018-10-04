91cf749394a974d9856dfb6a1866ae73eb193b6e
magicpro97 <linhnt99x@gmail.com>
2018-10-04 11:07:03 +0700

    add firebase

--- app/src/main/java/com/linh/wiinav/view/MapsActivity.java
+++ app/src/main/java/com/linh/wiinav/view/MapsActivity.java
@@ -1,7 +1,6 @@
 package com.linh.wiinav.view;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -36,7 +35,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
